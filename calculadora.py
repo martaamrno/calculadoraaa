737cde39e789dd35169cd20fbe31795e66816f87
martaamrno <morenodominguezmarta25@gmail.com>
2024-10-04 08:46:49 +0200

    añadimos division

--- calculadora.py
+++ calculadora.py
@@ -3,5 +3,8 @@ n2= int(input("introduce otro numero: "))
 
 suma = n1+n2
 resta= n1-n2
+multipl= n1*n2
+
 print("la suma es: " + str( suma))
 print("la resta es: ",resta)
+print("la multiplicacion es: ",multipl)
